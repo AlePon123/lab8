aa6003c1f43efdb278d996964a2b52fffce4848c
Пономарь Александр <alepon1337@mail.ru>
2024-10-05 07:21:34 +0300

    time

---
+++ time.pas
@@ -0,0 +1,7 @@
+﻿program t2;
+
+begin
+  var n:= ReadInteger();
+  if ((n mod 4) = 0) and ((n mod 100) <> 0) and ((n mod 400) <> 0) then Println('Високосный')
+  else Println('Не високосный');
+end.
