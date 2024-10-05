446b351edb8d9f5ba0b6b44d211730a2f22970f6
Пономарь Александр <alepon1337@mail.ru>
2024-10-05 07:22:56 +0300

    Changed time

--- time.pas
+++ time.pas
@@ -2,6 +2,6 @@
 
 begin
   var n:= ReadInteger();
-  if ((n mod 4) = 0) and ((n mod 100) <> 0) and ((n mod 400) <> 0) then Println('Високосный')
-  else Println('Не високосный');
+  if ((n mod 4) = 0) and ((n mod 100) <> 0) and ((n mod 400) <> 0) then Println('366')
+  else Println('365');
 end.
