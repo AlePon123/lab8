16b3134b502681ac076909be5c4afe88c9f76204
Пономарь Александр <alepon1337@mail.ru>
2024-10-05 07:15:13 +0300

    Changed hello

--- hello.pas
+++ hello.pas
@@ -1,5 +1,12 @@
 ﻿program t1;
 
 begin
-  Println('Hello world!');
+  var n:= ReadInteger();
+  assert((0<=n) and (n<=23));
+  case n of 
+    4..10: Println('Доброе утро, мир!');
+    11..16: Println('Добрый день, мир!');
+    17..22: Println('Добрый вечер, мир!');
+    else Println('Доброй ночи, мир!');
+  end;
 end.
